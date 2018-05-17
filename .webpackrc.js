6e9a567c80566dbef6d9f533dfe40ddac709d99f
philipgreat <philip_chang@163.com>
2018-05-18 01:45:22 +0800

    update framework

--- .webpackrc.js
+++ .webpackrc.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 export default {
   entry: 'src/index.js',
-  extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+  extraBabelPlugins: [
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
+  ],
   env: {
     development: {
       extraBabelPlugins: ['dva-hmr'],
