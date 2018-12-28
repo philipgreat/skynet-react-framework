fe06fa4db40f1ea34af721981ad2a2a1b89a7b1f
philipgreat <philip_chang@163.com>
2018-12-28 22:53:32 +0800

    update to new version

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 //savefileifnotexist
-export const SYSTEM_SHORT_NAME = "moyi" //Use for requssting different apps
-export const SYSTEM_LOCAL_NAME = "Art0X社区" 
-export const BUILD_TIIME="Tue Dec 11 11:17:23 CST 2018"
+export const SYSTEM_SHORT_NAME = "retailscm" //Use for requssting different apps
+export const SYSTEM_LOCAL_NAME = "双链小超全流程供应链系统" 
+export const BUILD_TIIME="Tue Dec 18 00:33:06 CST 2018"
 export const MEDIA_UPLOAD_URL="https://www.doublechaintech.com/mss/upload.html"
 export const MEDIA_PREFIX="https://www.doublechaintech.com/mss/"
 
