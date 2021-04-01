1d73920d1e444bcc3feca25498eca81406229cfe
Philip Zhang <zhangxilai@doublechaintech.com>
2021-04-01 13:37:32 +0800

    Sync with dev work

--- src/components/HTMLEditor/index.js
+++ src/components/HTMLEditor/index.js
@@ -1,35 +1,28 @@
-import React, { useState,useMemo,useRef } from "react";
-import ReactQuill,{ Quill }  from 'react-quill';
+import React, { useMemo, useRef } from 'react';
+import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import ImageService from '../../services/ImageService'
-import {ImageDrop} from 'quill-image-drop-module'
-import ImageResize from '@taoqf/quill-image-resize-module'
+import ImageService from '../../services/ImageService';
+import ImageResize from '@taoqf/quill-image-resize-module';
 
-Quill.register('modules/imageResize', ImageResize)
-
+Quill.register('modules/imageResize', ImageResize);
 
 
-const container=[
-  
+const container = [
+
   [{ size: [] }],
   ['bold', 'italic', 'underline', 'strike', 'blockquote'],
   [{ list: 'ordered' }, { list: 'bullet' }],
   ['link', 'image'],
   ['clean'],
-  ['code-block']
-]
+  ['code-block'],
+];
 
 
-
-
-
-
-function selectLocalImage(param,quillRef) {
-  console.log("selectLocalImage", param,quillRef)
+function selectLocalImage(param, quillRef) {
+  console.log('selectLocalImage', param, quillRef);
   const input = document.createElement('input');
   input.setAttribute('type', 'file');
   input.click();
-
 
 
   // Listen upload local image and save to server
@@ -39,37 +32,37 @@ function selectLocalImage(param,quillRef) {
     // file type is only image.
     if (/^image\//.test(file.type)) {
       //saveToServer(file);
-      const quillEditor = quillRef.current.getEditor()
+      const quillEditor = quillRef.current.getEditor();
       const range = quillEditor.getSelection(true);
       const reader = new FileReader();
       reader.readAsDataURL(file);
 
       //reference https://quilljs.com/docs/api/
 
-      reader.onload = () =>{
-        const {uploadOneImage} = ImageService
-        const image ={name:"noname", mimeType:file.type, encodeContent: reader.result}
+      reader.onload = () => {
+        const { uploadOneImage } = ImageService;
+        const image = { name: 'noname', mimeType: file.type, encodeContent: reader.result };
         quillEditor.insertText(range.index, '正在上传.....', 'bold', true);
-        quillEditor.disable()
-        uploadOneImage({...image}).then((response)=>{
-          if(!response.class){
-            alert("图片上传失败(response.class)!")
-            return
+        quillEditor.disable();
+        uploadOneImage({ ...image }).then((response) => {
+          if (!response.class) {
+            alert('图片上传失败(response.class)!');
+            return;
           }
-          if(!response.class.indexOf("ImageUploadResponse")<0){
-            alert("图片上传失败!-ImageUploadResponse")
-            return
+          if (!response.class.indexOf('ImageUploadResponse') < 0) {
+            alert('图片上传失败!-ImageUploadResponse');
+            return;
           }
           const imageId = response.remoteUrl;
-          
+
           quillEditor.deleteText(range.index, '正在上传.....'.length);
           quillEditor.insertEmbed(range.index, 'image', `${imageId}`);
-          quillEditor.enable(true)
-        })
-        
+          quillEditor.enable(true);
+        });
 
-      }
-      console.log("File", file.type)
+
+      };
+      console.log('File', file.type);
 
     } else {
       console.warn('You could only upload images.');
@@ -78,24 +71,29 @@ function selectLocalImage(param,quillRef) {
 }
 
 
-function RichEditor(props) {
-  const [value, setValue] = useState('<div>hello</div>');
+export default function RichEditor(props) {
+
+  const { value, onChange } = props;
+
   const quillRef = useRef(null);
   const modules = useMemo(() => ({
-    imageResize: {
-    },
+    imageResize: {},
     toolbar: {
       container: container,
-      handlers: {image: (value)=>selectLocalImage(value,quillRef)}
-    }
-  }), [])
-
-  
+      handlers: { image: (value) => selectLocalImage(value, quillRef) },
+    },
+  }), []);
 
 
   return (
-    <ReactQuill ref={quillRef} style={{minHeight:"100px",height:"600px",}} modules={modules} theme="snow" value={value} onChange={setValue}/>
+    <ReactQuill
+      ref={quillRef}
+      style={{ minHeight: '300px', height: '400px' }}
+      modules={modules}
+      theme='snow'
+      value={value}
+      onChange={onChange} />
   );
 }
 
-export default RichEditor
+
--- src/components/RichEditInput/index.js
+++ src/components/RichEditInput/index.js
@@ -1,65 +1,90 @@
-import { Input, Select, Button,Drawer } from 'antd';
-import React from 'react'
-import HTMLEditor from '../HTMLEditor'
-const { Option } = Select;
+import { Button, Drawer, Modal } from 'antd';
+import React, { forwardRef, useState } from 'react';
+import _ from 'lodash';
+import HTMLEditor from '../HTMLEditor';
+import styles from './index.less';
 
-export default class RichEditInput extends React.Component {
-  state = { drawerVisible: true };
-  handleNumberChange = e => {
-    const number = parseInt(e.target.value || 0, 10);
-    if (isNaN(number)) {
+
+function RichEditInput(props) {
+  const [drawerVisible, setDrawerVisible] = useState(false);
+  const [tipPopupVisible, setTipPopupVisible] = useState(false);
+  const [content, setContent] = useState();
+
+  const { value, onChange } = props;
+
+
+  const showDrawer = () => setDrawerVisible(true);
+  const closeDrawer = () => setDrawerVisible(false);
+  const showTipsPopup = () => setTipPopupVisible(true);
+  const closeTipsPopup = () => setTipPopupVisible(false);
+  const resetValue = () => setContent(value);
+  const isTheContentChange = () => _.isString(content) && !_.isEmpty(content) && value !== content;
+
+  const handleCloseDrawer = () => {
+    if (isTheContentChange()) {
+      showTipsPopup();
       return;
     }
-    this.triggerChange({ number });
+    closeDrawer();
+
   };
 
-
-  handleCurrencyChange = currency => {
-    this.triggerChange({ currency });
-  };
-
-  triggerChange = changedValue => {
-    const { onChange, value } = this.props;
-    if (onChange) {
-      onChange({
-        ...value,
-        ...changedValue,
-      });
+  const handleSave = () => {
+    if (onChange instanceof Function) {
+      onChange(content);
+      closeTipsPopup();
+      closeDrawer();
     }
   };
-  showDrawer = () => {
-    this.setState({
-      drawerVisible: true,
-    });
+
+  const handleCancel = () => {
+    resetValue();
+    closeTipsPopup();
+    closeDrawer();
   };
 
-  onClose = () => {
-    console.log("onClose triggered")
-    this.setState({
-      drawerVisible: false,
-    });
-  };
-  render() {
-    const { size, value } = this.props;
-    return (
-      <div>
-        <div style={{width:"600px", height:"200px", border:"solid 1px blue",padding:"10px"}}
-          dangerouslySetInnerHTML={{
-            __html: value.htmlText
-          }}></div>
-        <Button type="primary" icon="edit" onClick={this.showDrawer}>{'编辑'}</Button>
-        <Drawer
-          title="编辑内容"
-          placement="right"
-          closable={true}
-          onClose={this.onClose}
-          visible={this.state.drawerVisible}
-          
-          width={720}
-        >
-          <HTMLEditor />
-        </Drawer>
+
+  const theContent = content || value;
+
+  return (
+    <div className={styles.richEditInput}>
+      <div className={styles.contentPreview}>
+        <div className={styles.contentPreviewTxt} >
+          <div dangerouslySetInnerHTML={{ __html: value }} />
+        </div>
+        <Button className={styles.contentEditButton} type='dashed' icon='edit' onClick={showDrawer}>编辑</Button>
       </div>
-    );
-  }
+
+      <input style={{ display: 'none' }} value={value} />
+
+      <Modal
+        visible={tipPopupVisible}
+        title='取消编辑'
+        onCancel={closeTipsPopup}
+        footer={[
+          <Button onClick={closeTipsPopup}>取消</Button>,
+          <Button type='primary' onClick={handleSave}>保存</Button>,
+          <Button type='danger' onClick={handleCancel}>放弃修改</Button>,
+        ]}
+      >
+        内容未保存，是否放弃本次修改?
+      </Modal>
+
+      <Drawer
+        title='编辑内容'
+        placement='right'
+        closable={false}
+        visible={drawerVisible}
+        width={720}
+      >
+        <HTMLEditor value={theContent} onChange={setContent} />
+        <div className={styles.popupDrawerActionList}>
+          <Button onClick={handleSave} type='primary'>确定</Button>
+          <Button onClick={handleCloseDrawer}>取消</Button>
+        </div>
+      </Drawer>
+    </div>
+  );
 }
+
+export default forwardRef(RichEditInput);
